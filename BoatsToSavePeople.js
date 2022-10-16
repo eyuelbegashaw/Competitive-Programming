f307dd7997ed18a349a2d62d03d8b62c6891271c
Eyuel Begashaw <eyuelbegashaw214@gmail.com>
2022-10-16 13:21:07 +0300

    Create BoatsToSavePeople.js

---
+++ BoatsToSavePeople.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+var numRescueBoats = function(people, limit) {
+    people.sort((a,b) => a-b);
+    let i=0 , j = people.length-1 , boats=0;
+   
+    while(i<=j) {
+        boats++;
+        if(people[i] + people[j] <= limit) 
+            i++;
+        j--;   
+     }
+    
+     return boats;
+};
+    
+    
